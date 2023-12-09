20deccd15ffd92d2846e7563bf94f09b2aa63b77
Ferdhy Ihza Armanda <ferdhyia.dev@gmail.com>
2023-12-09 19:51:09 +0700

    integration admin dashboard

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Models\Checkout;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\User\DashboardController as UserDashboard;
 use App\Http\Controllers\Admin\DashboardController as AdminDashboard;
+use App\Http\Controllers\Admin\CheckoutController as AdminCheckout;
 
 /*
 |--------------------------------------------------------------------------
@@ -45,6 +46,8 @@ Route::middleware(['auth'])->group(function () {
     // admin dashboard
     Route::prefix('admin/dashboard')->namespace('Admin')->name('admin.')->middleware('ensureUserRole:admin')->group(function(){
         Route::get('/', [AdminDashboard::class, 'index'])->name('dashboard');
+        // admin checkout
+        Route::post('checkout/{checkout}', [AdminCheckout::class, 'update'])->name('checkout.update');
     });
 });
 
